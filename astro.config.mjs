6d031ef1e2c4cf9aaf34d9d38571bee78170943b
vanessa <vanessapcbarros@gmail.com>
2023-03-04 18:34:06 +0100

    install sitemap package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()],
 });
