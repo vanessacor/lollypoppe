3e5aea12e7a2a982f076fc5751af3f792224ac30
vanessa <vanessapcbarros@gmail.com>
2023-03-04 18:37:55 +0100

    scaffold blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,8 @@ const blogCollection = defineCollection({
     tags: z.array(z.string()),
     image: z.string().optional(),
     updateDate: z.string().transform((str) => new Date(str)),
+    description: z.string(),
+    draft: z.boolean().optional(),
   }),
 });
 const projectCollection = defineCollection({
