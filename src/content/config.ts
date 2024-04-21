f2d0214efbf20f705b48a532b40f53ea9513d157
vanessa <vanessapcbarros@gmail.com>
2024-04-21 11:49:02 +0200

    remove project leftovers

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,21 +10,9 @@ const blogCollection = defineCollection({
     draft: z.boolean().optional(),
   }),
 });
-const projectCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    link: z.string(),
-    image: z.string(),
-    tags: z.array(z.string()),
-    gitHubLink: z.string(),
-    date: z.string().transform((str) => new Date(str)),
-    deploy: z.string(),
-  }),
-});
 
 export const collections = {
   blog: blogCollection,
-  projects: projectCollection,
 };
 
 export const glossaryCollection = defineCollection({
