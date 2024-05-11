d4aaaf7026674fabd6fd6be55345a657c30d8816
vanessa <vanessapcbarros@gmail.com>
2024-05-11 11:16:05 +0200

    fix schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { z, defineCollection } from "astro:content";
 
 const blogCollection = defineCollection({
+  type: "content",
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
@@ -11,22 +12,26 @@ const blogCollection = defineCollection({
   }),
 });
 
-export const collections = {
-  blog: blogCollection,
-};
-
 export const glossaryCollection = defineCollection({
   schema: z.object({
     term: z.string(),
   }),
 });
 
-const workCollection = defineCollection({
+export const workCollection = defineCollection({
+  type: "content",
   schema: z.object({
     id: z.string(),
     company: z.string(),
     jobTitle: z.string(),
     location: z.string(),
-    dates: z.array(z.string()),
+    techSkills: z.array(z.string()).optional(),
+    softSkills: z.array(z.string()).optional(),
+    dates: z.string(),
   }),
 });
+
+export const collections = {
+  blog: blogCollection,
+  work: workCollection,
+};
