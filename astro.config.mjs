aabe4c763d87d76c67f839740d9d9c087ec25147
vanessa <vanessapcbarros@gmail.com>
2024-07-06 15:01:56 +0200

    install solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { externalLinks } from "./src/integration/rehype/externalLinks";
+import solidJs from "@astrojs/solid-js";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), solidJs()],
   markdown: {
     rehypePlugins: [externalLinks],
     shikiConfig: {
       theme: "rose-pine-dawn",
       langs: ["js", "ts", "html", "bash", "json"],
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
